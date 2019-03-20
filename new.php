71281e657ae90f800f07e661b0480b28ddd50511
Tavo Soto <gsoto@itx.com>
2019-03-20 10:10:14 -0500

    fixing a markup error

--- new.php
+++ new.php
@@ -10,14 +10,17 @@
 				echo "<label>Name</label><br/>";
 				echo "<input type='text' name='name' />";
 			echo "</div>";
+
 			echo "<div class='field-group'>";
 				echo "<label>Email</label><br/>";
 				echo "<input type='text' name='email' />";
 			echo "</div>";
+
 			echo "<div class='field-group'>";
-				echo "<label>Message</lable><br/>";
+				echo "<label>Message</label><br/>";
 				echo "<textarea name='message'></textarea>";
 			echo "</div>";
+
 			echo "<input type='submit' />";
 		echo "</form>";
 
