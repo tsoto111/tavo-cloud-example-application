e554847530122ad620082a5040446761b2fbc489
Tavo Soto <gsoto@itx.com>
2019-03-20 09:50:46 -0500

    Re-formatting

--- single.php
+++ single.php
@@ -1,30 +1,41 @@
 <?php
+
 	include("common.php");
+	
 	$dbh = getReadOnlyConnection();
+	
 	$stmt = $dbh->prepare(
 		"select * from gb_entries" .
 		" where id = :id"
 	);
+	
 	$stmt->bindValue(
 		":id", 
 		$_GET["id"], 
 		PDO::PARAM_INT
 	);
+	
 	$stmt->execute();
+	
 	$result = $stmt->fetchAll();
+	
 	$row = $result[0];
+	
 	getHeader();
-?>
-<p>
-	<a href="list.php">Go Back</a>
-</p>
 
-<label>Name</label> 
-	<?php echo h($row["name"]) ?>
-<label>Email</label> 
-	<?php echo h($row["email"]) ?>
-<label>Message</label> 
-	<?php echo h($row["message"]) ?>
-<?php
+	echo "<p>";
+		echo "<a href='list.php'>Go Back</a>";
+	echo "</p>";
+
+	echo "<label>Name</label>";
+	echo h($row["name"]);
+
+	echo "<label>Email</label>";
+	echo h($row["email"]);
+
+	echo "<label>Message</label>";
+	echo h($row["message"]);
+
 	getFooter();
+
 ?>
