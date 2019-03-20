f8782607b5b50631b145f67102a404c9c353ed9d
Tavo Soto <gsoto@itx.com>
2019-03-20 09:35:15 -0500

    Modifying common.php code to be more readable for me.

--- common.php
+++ common.php
@@ -1,37 +1,40 @@
-<?php
+<?php 
+
 	function getReadOnlyConnection() {
-		return new PDO("pgsql:host=localhost;" .
-			"port=5432;dbname=guestbookapp;" .
+		return new PDO(
+			"pgsql:host=localhost;".
+			"port=5432;dbname=guestbookapp;".
 			"user=gbuser;password=mypassword"
 		);
 	}
+
 	function getReadWriteConnection() {
-		return new PDO("pgsql:host=localhost;" .
-			"port=5432;dbname=guestbookapp;" .
+		return new PDO(
+			"pgsql:host=localhost;".
+			"port=5432;dbname=guestbookapp;".
 			"user=gbuser;password=mypassword"
 		);
 	}
+
 	function getHeader() {
-?>
-<html>
-	<head>
-		<title>Guestbook App</title>
-		<link rel="stylesheet" 
-			href="guestbook.css" />
-	</head>
-	<body>
-		<h1>Guestbook App</h1>
-		<div id="main">
-<?php
+		echo "<html>";
+			echo "<head>";
+				echo "<title>Guestbook App</title>";
+				echo "<link rel='stylesheet' href='guestbook.css' />";
+			echo "</head>";
+			echo "<body>";
+				echo "<h1>Guestbook App</h1>";
+				echo "<div id='main'>";
 	}
+
 	function getFooter() {
-?>
-		</div>
-	</body>
-</html>
-<?php
+				echo "</div>";
+			echo "</body>";
+		echo "</html>";
 	}
+
 	function h($str) {
 		return htmlspecialchars($str);
 	}
+
 ?>
